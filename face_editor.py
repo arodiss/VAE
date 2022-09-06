4bb8ea0a1396862b3eda0921e7bbc5494c056e9c
Gleb Sidora <gleb.sidora@gmail.com>
2022-09-06 20:36:20 +0100

    updated model

--- face_editor.py
+++ face_editor.py
@@ -9,7 +9,7 @@ import torch
 
 class FaceEditor:
     def __init__(self, raw_face_array):
-        self.change = [0, 0, 0, 0]
+        self.change = [0, 0, 0, 0, 0, 0, 0]
         self.model = get_best_model()
         self.root = Tk()
         self.root.title("Face editor")
@@ -22,6 +22,9 @@ class FaceEditor:
         self.slider1 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change1)
         self.slider2 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change2)
         self.slider3 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change3)
+        self.slider4 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change4)
+        self.slider5 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change5)
+        self.slider6 = Scale(from_=-100, to=100, orient=HORIZONTAL, command=self.change6)
 
         raw_img = Image.fromarray(np.uint8(cm.gray(raw_face_array) * 255)).resize((300, 300))
         raw_tk_img = ImageTk.PhotoImage(raw_img)
@@ -33,6 +36,9 @@ class FaceEditor:
         self.slider1.pack()
         self.slider2.pack()
         self.slider3.pack()
+        self.slider4.pack()
+        self.slider5.pack()
+        self.slider6.pack()
         self.update_image()
 
         self.root.mainloop()
@@ -48,19 +54,30 @@ class FaceEditor:
         self.reconstructed_image_container.create_image(0, 0, image=self.reconstructed_tk_img, anchor=NW)
 
     def change0(self, var):
-        self.change[0] = float(var) * 0.01
+        self.change[0] = float(var) * 0.1
         self.update_image()
 
     def change1(self, var):
-        self.change[1] = float(var) * 0.01
+        self.change[1] = float(var) * 0.1
         self.update_image()
 
     def change2(self, var):
-        self.change[2] = float(var) * 0.01
+        self.change[2] = float(var) * 0.1
         self.update_image()
 
     def change3(self, var):
-        self.change[3] = float(var) * 0.01
+        self.change[3] = float(var) * 0.1
+        self.update_image()
+
+    def change4(self, var):
+        self.change[4] = float(var) * 0.1
+        self.update_image()
+
+    def change5(self, var):
+        self.change[5] = float(var) * 0.1
+        self.update_image()
+    def change6(self, var):
+        self.change[6] = float(var) * 0.1
         self.update_image()
 
 
